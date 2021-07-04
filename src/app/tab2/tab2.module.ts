78027999e0cd6a5430ac9290af19e83ccb4c1cb3
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-07-04 18:11:27 -0300

    primeiro commit

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,5 +1,4 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -18,4 +17,5 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule {
+}
